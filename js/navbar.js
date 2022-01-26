3c4359595c9cd252858753270f3d10385c936f0d
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-01-26 11:49:52 +0100

    show > slide animation

--- js/navbar.js
+++ js/navbar.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     $("body > header > nav > ul > li:first-of-type > button").click(function(){
         if (!$(this).hasClass("navActive")){
             $(this).addClass("navActive");
-            $("body > header > nav > div").show();
+            $("body > header > nav > div").slideDown();
             $("main").css("opacity", "0.2");
             
         } 
         else{
             $(this).removeClass("navActive");
-            $("body > header > nav > div").hide();
+            $("body > header > nav > div").slideUp();
             $("main").css("opacity", "1.0");
         }
     });
