0e900cd0913a0487c1a673652c5394fcfb1f3f52
Matteo <matteo.violani@studio.unibo.it>
2022-01-27 17:35:33 +0100

    switch form disable/enable editing fields address

--- js/payment.js
+++ js/payment.js
@@ -1,8 +1,16 @@
 function editAddress(){
     $(document).ready(function () {
-        var fields = $("main > section > div > div > article > form > ul > li > input");
-        fields.attr('disabled', false);
-        $("main > section > div > div > article > form > a:first-of-type").hide();
-        $("main > section > div > div > article > form > a:last-of-type").show();
+        var fields = $("main > section > div > div > article > form > ul > li > input:not(:first-child)");
+        if($("main > section > div > div > article > form > a:first-of-type").is(":visible")){
+            fields.attr('disabled', false);
+            $("main > section > div > div > article > form > a:first-of-type").hide();
+            $("main > section > div > div > article > form > a:last-of-type").show();
+        }
+        else{
+            fields.attr('disabled', true);
+            $("main > section > div > div > article > form > a:first-of-type").show();
+            $("main > section > div > div > article > form > a:last-of-type").hide();
+        }
+        
     });    
 }
