b5d99535d53265ddb593746bfd7c042fc971a5d9
Matteo <matteo.violani@studio.unibo.it>
2022-01-27 17:24:43 +0100

    js to edit fields in payments page

--- js/payment.js
+++ js/payment.js
@@ -1,4 +1,8 @@
-$("").click(function (e) { 
-    
-    
-});
+function editAddress(){
+    $(document).ready(function () {
+        var fields = $("main > section > div > div > article > form > ul > li > input");
+        fields.attr('disabled', false);
+        $("main > section > div > div > article > form > a:first-of-type").hide();
+        $("main > section > div > div > article > form > a:last-of-type").show();
+    });    
+}
