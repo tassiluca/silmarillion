df2c88c29bf781ab7d4723112a102cdda65522d7
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-01-27 09:12:11 +0100

    refactor common code

--- js/navbar.js
+++ js/navbar.js
@@ -4,35 +4,32 @@ $(document).ready(function(){
 
     /* Menu button clicked */
     $("body > header > nav > ul > li:first-of-type > button").click(function(){
-        if (!$(this).hasClass("navActive")){
-            $(this).addClass("navActive");
-            $("body > header > nav > div.navMenu").slideDown();
-            /* add opacity to the main page */
-            $("main").css("opacity", "0.2");
-        } 
-        else {
-            $(this).removeClass("navActive");
-            $("body > header > nav > div.navMenu").slideUp();
-            /* restore the opacity of the main page */
-            $("main").css("opacity", "1.0");
-        }
+        toggleNavbar($(this), $("body > header > nav > div.navMenu"));
     });
 
     /* Cart button clicked */
     $("body > header > nav > ul > li:last-of-type > button").click(function(){
-        if (!$(this).hasClass("navActive")){
-            $(this).addClass("navActive");
-            $("body > header > nav > div.navCart").show();
-            $("main").css("opacity", "0.2");
-        } 
-        else {
-            $(this).removeClass("navActive");
-            $("body > header > nav > div.navCart").hide();
-            $("main").css("opacity", "1.0");
-        }
+        toggleNavbar($(this), $("body > header > nav > div.navCart"));
     });
-
-
 
 });
 
+/**
+ * Toggle one element of navbar
+ * @param {*} btnPressed the navbar button which has been pressed
+ * @param {*} elementToSlide the section of page to show
+ */
+function toggleNavbar(btnPressed, elementToSlide){
+    if (!btnPressed.hasClass("navActive")){
+        /* add the class navActive in order to color the button pressed */
+        btnPressed.addClass("navActive");
+        elementToSlide.slideDown();
+        /* add opacity to the main page */
+        $("main").css("opacity", "0.2");
+    } else {
+        btnPressed.removeClass("navActive");
+        elementToSlide.slideUp();
+        /* restore the opacity of the main page */
+        $("main").css("opacity", "1.0");
+    }
+}
