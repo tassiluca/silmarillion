f03fcd56449e824792d5d8af2d269d62fc701753
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-01-27 08:56:42 +0100

    added comments

--- js/navbar.js
+++ js/navbar.js
@@ -1,40 +1,38 @@
 $(document).ready(function(){
 
-    /*$("body > header > nav > div").hide();*/
-var pressedBtn = false;
+    var pressedBtn = false;
+
+    /* Menu button clicked */
     $("body > header > nav > ul > li:first-of-type > button").click(function(){
         if (!$(this).hasClass("navActive")){
             $(this).addClass("navActive");
             $("body > header > nav > div.navMenu").slideDown();
+            /* add opacity to the main page */
             $("main").css("opacity", "0.2");
-            
         } 
-        else{
+        else {
             $(this).removeClass("navActive");
             $("body > header > nav > div.navMenu").slideUp();
+            /* restore the opacity of the main page */
             $("main").css("opacity", "1.0");
         }
     });
 
-    /* cart navbar*/
+    /* Cart button clicked */
     $("body > header > nav > ul > li:last-of-type > button").click(function(){
-        
         if (!$(this).hasClass("navActive")){
             $(this).addClass("navActive");
             $("body > header > nav > div.navCart").show();
             $("main").css("opacity", "0.2");
         } 
-        else{
+        else {
             $(this).removeClass("navActive");
             $("body > header > nav > div.navCart").hide();
             $("main").css("opacity", "1.0");
         }
     });
-/*
-    function resetIfPressed(){
-            $("body > header > nav > ul > li > button").removeClass("navActive");
-            $("body > header > nav > div").hide();
-    }
-    */
+
+
+
 });
 
