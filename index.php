750f11aa4c9097210bf29f785bfa222ac4169ec4
Matteo <matteo.violani@studio.unibo.it>
2022-01-31 10:35:18 +0100

    added css homepage article template home

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
     //require_once 'bootstrap.php';
 
-    $templateParams["css"] = array("homepage.css");
+    $templateParams["css"] = array("homepage.css","product.css");
     $templateParams["js"] = array("home.js");
     $templateParams["main"] = "./templates/home-page.php";
-
 
     require 'templates/base.php';
 ?>
