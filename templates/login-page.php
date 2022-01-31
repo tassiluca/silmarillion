2feccf1c0ba45abccd83be230a3e87be62972956
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-01-31 10:17:26 +0100

    refactor html structure forms. adapted style sheets

--- templates/login-page.php
+++ templates/login-page.php
@@ -5,7 +5,7 @@
                     <li><a href="index.html"><img src="./img/home-icon.svg" alt="Home"></a></li><li>Login</li>
                 </ul>
             </div>
-            <form action="#" method="POST" onsubmit="formhash(this, this.userpassword);">
+            <section>
                 <header>
                     <div>
                         <img src="./img/login/user-men-icon.svg" alt="">
@@ -15,37 +15,41 @@
                     </ul>
                 </header>
                 <!-- User login form -->
-                <ul id="userlogin">
-                    <li>
-                        <label for="username">Username</label>
-                        <input type="text" placeholder="es. giovanni.delnevo@gmail.com" id="username" name="usr" required />
-                    </li>
-                    <li>
-                        <label for="userpassword">Password</label>
-                        <input type="password" placeholder="Password" id="userpassword" name="userPwd" required />
-                    </li>
-                    <li>
-                        <a href="">Hai dimenticato la password?</a>
-                        <input type="submit" name="submit" value="ACCEDI" />
-                    </li>
-                    <li>
-                        <p>Non sei registrato?</p>
-                        <a href="./register.html">CREA IL TUO ACCOUNT</a>
-                    </li>
-                </ul>
+                <form action="#" method="POST" onsubmit="formhash(this, this.userpassword);">
+                    <ul id="userlogin">
+                        <li>
+                            <label for="username">Username</label>
+                            <input type="text" placeholder="es. giovanni.delnevo@gmail.com" id="username" name="usr" required />
+                        </li>
+                        <li>
+                            <label for="userpassword">Password</label>
+                            <input type="password" placeholder="Password" id="userpassword" name="userPwd" required />
+                        </li>
+                        <li>
+                            <a href="">Hai dimenticato la password?</a>
+                            <input type="submit" name="submit" value="ACCEDI" />
+                        </li>
+                        <li>
+                            <p>Non sei registrato?</p>
+                            <a href="./register.html">CREA IL TUO ACCOUNT</a>
+                        </li>
+                    </ul>
+                </form>
                 <!-- Seller login form -->
-                <ul id="sellerlogin">
-                    <li>
-                        <label for="sellername">Username</label>
-                        <input type="text" placeholder="es. silvia.mirri@silmarillion.com" id="sellername" name="sellerUsr" />
-                    </li>
-                    <li>
-                        <label for="sellerpassword">Password</label>
-                        <input type="password" placeholder="Password" id="sellerpassword" name="sellerPwd" />
-                    </li>
-                    <li>
-                        <a href="">Hai dimenticato la password?</a>
-                        <input type="submit" name="submit" value="ACCEDI" />
-                    </li>
-                </ul>
+                <form action="#" method="POST" onsubmit="formhash(this, this.userpassword);">
+                    <ul id="sellerlogin">
+                        <li>
+                            <label for="sellername">Username</label>
+                            <input type="text" placeholder="es. silvia.mirri@silmarillion.com" id="sellername" name="sellerUsr" />
+                        </li>
+                        <li>
+                            <label for="sellerpassword">Password</label>
+                            <input type="password" placeholder="Password" id="sellerpassword" name="sellerPwd" />
+                        </li>
+                        <li>
+                            <a href="">Hai dimenticato la password?</a>
+                            <input type="submit" name="submit" value="ACCEDI" />
+                        </li>
+                    </ul>
+                </form>
             </form>
