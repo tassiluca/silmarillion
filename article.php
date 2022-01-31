603450ff10a7d35fcee399bc2fa44b64ef2b0e3c
Matteo <matteo.violani@studio.unibo.it>
2022-01-31 18:24:12 +0100

    init php for article(comic) page

---
+++ article.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'bootstrap.php';
+
+    $templateParams["css"] = array("article.css");
+    $templateParams["js"] = array("");
+    $templateParams["main"] = "./templates/single-article.php";
+
+    require 'templates/base.php';
+?>
---
+++ templates/single-article.php
@@ -0,0 +1,39 @@
+<div>
+                <ul>
+                    <li><a href="index.html"><img src="./img/home-icon.svg" alt="Home"></a></li><li><a href="catalog.html">Catalogo</a></li><li>The seven deathly sins 1</li>
+                </ul>
+            </div>
+            <h2>The seven deathly sins 1</h2>
+            <aside>
+                <div>
+                    <img src="./img/fumetto2.jpg" alt="">
+                </div>
+            </aside><section>
+                <header>
+                    <ul id="productInfo">
+                        <!-- NOTE: if the article is not available implement "soldout" class, otherwise "available" class -->
+                        <!-- TODO: implement link with php -->
+                        <li class="soldout">Non disponibile</li><li><a href="#notificationArea"><img src="./img/favourite.svg" alt="Preferito" /></a></li>
+                        <li><a href="#notificationArea">Avvisami quando questo prodotto sarà disponibile</a></li>
+                    </ul>
+                    <div id="notificationArea" class="warning">
+                        <!-- NOTE: if an error message must be displayed add a class error, otherwise message -->
+                        <span></span>
+                        <strong>Per svolgere questa azione devi essere loggato!</strong>
+                    </div>
+                    <ul>
+                        <li><strong>Autore:</strong> Nakaba Suzuki</li>
+                        <li><strong>Editore:</strong> Star Comics</li>
+                        <li><strong>Anno:</strong> 2014</li>
+                        <li><strong>Lingua:</strong> Italiano</li>
+                        <li><strong>ISBN:</strong> 817525766-0</li>
+                    </ul>
+                </header>
+                <p><strong>Descrizione:</strong> Si narra che sette temibili guerrieri, conosciuti come i Seven Deadly Sins, tramarono per rovesciare un pacifico regno nelle terre di Britannia.
+                Da allora nessuno li ha più visti. Dieci anni dopo, convinta della loro innocenza e sicura che siano sopravvissuti, una giovane fanciulla si mette sulle loro tracce: nuovi temibili nemici minacciano il regno e i sette "peccatori" sono la sua unica speranza! Inizia così una nuova imperdibile avventura fantasy coinvolgente e ricca di azione, magia e colpi di scena!</p>
+                <footer>
+                    <ul>
+                        <li><span>16,00 €</span> 4,30 €</li><li><a href="#notificationArea"><span>Aggiungi al carrello</span><img src="./img/add-to-cart.svg" alt="Aggiungi al carrello"></a></li>
+                    </ul>
+                </footer>
+            </section>
