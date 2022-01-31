4ec1668de00f380da1727f771a53fbe051f1cc42
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-01-31 05:29:46 +0100

    init db php

---
+++ db/database.php
@@ -0,0 +1,38 @@
+<?php
+
+    class DatabaseHelper {
+        private $db;
+
+        public function __construct($servername, $username, $password, $dbname, $port) {
+            $this->db = new mysqli($servername, $username, $password, $dbname, $port);
+            if ($this->db->connect_error) {
+                die("Connection failed: " . $this->db->connect_error);
+            }
+        }
+
+        public function getUserData($username) {
+            $query = "SELECT UserId, Password, Salt, IsActive
+                      FROM Users 
+                      WHERE Username = ?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function getLoginAttempts($userId, $validAttempts) {
+            /* Please note validAttempts is generated from the system. */
+            $query = "SELECT TimeLog
+                      FROM LoginAttemps 
+                      WHERE UserId = ?
+                      AND TimeLog > '$validAttempts'";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('i', $userId);
+            $stmt->execute();
+            return $stmt->get_result()->num_rows;
+        }
+
+    }
+
+?>
