778a2fecb8c1c21ecf92d42047e44d48a6b8a5c4
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-02 00:14:23 +0100

    update account

--- db/sqlScript/populate-tables.sql
+++ db/sqlScript/populate-tables.sql
@@ -1,8 +1,8 @@
-/* Nome utente: test_user
- * Indirizzo E-mail: test@example.com
- * Password: 6ZaxN2Vzm9NUJT2y
+/* Nome utente: silmarillion
+ * Password: Admin000
  */
+ /* NOTE THAT TABLE USERS AND SELLERS MUST BE EMPTY */
 INSERT INTO `silmarillion`.`Users` 
-VALUES(1, 'test_user', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef', 'usr', 'test', '2000-11-28', 'test@example.com', 1);
+VALUES(1, 'silmarillion', '9d5293b2cf3f2455a53d580196380f3abcdb95c7de1c37c0ddfa758a6ac21ffef6e5f784e517e5e7a2380c0659967360ca9b36140ce5c10214649b59df75a4d1', 'ed7e398663d4f017e871802c80cca67cbcbe8ff4ee3b23da59f2a566a36f60273c50121cbf148639d8c5bcf1bf312ee3c69b4328e2d1c723261897f7bfa16dea', 'admin', 'admin', '2000-01-01', 'test@example.com', 1);
 INSERT INTO `silmarillion`.`Sellers` 
 VALUES(1);
