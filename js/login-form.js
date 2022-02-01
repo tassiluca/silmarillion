b93c2cfd907459cb6fc49516c16b769595eef23b
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-01 13:28:13 +0100

    added doc

--- js/login-form.js
+++ js/login-form.js
@@ -1,4 +1,11 @@
+/**
+ * Makes a new login attempt and displays errors if something wrong.
+ * @param {form} form the login form submitted
+ * @param {JSON} formData the login form data inserted by the user
+ * @param {string} target the target page to which send the user if login success
+ */
 function loginAttempt(form, formData, target) {
+    // Clears errors results of previous attempts
     $(".hasError").removeClass("hasError");
     $(".error").remove();
 
@@ -9,9 +16,11 @@ function loginAttempt(form, formData, target) {
         dataType: "json",
         encode: true
     }).done(function (data) {
-        if (!data.success) { // there was an error
+        if (!data.success) { // if has been occured some error
+            // add class hasError to both username and password inputs
             $(form).find("ul > li:first-of-type").addClass("hasError");
             $(form).find("ul > li:nth-of-type(2)").addClass("hasError");
+            // add an error message
             if (data.errors.forcing) {
                 $(form).find("ul > li:nth-of-type(2)").append (
                     '<div class="error">' + data.errors.forcing + '</div>'
@@ -22,11 +31,12 @@ function loginAttempt(form, formData, target) {
                 );
             }
         } else {
+            // send the user to target
             window.location.href = target;
         }
-    }).fail(function(data) {
+    }).fail(function(data) { // error connecting to the server
         $(form).find("ul > li:nth-of-type(2)").append (
-            '<div class="error">Errore connessione db! Riprova...</div>'
+            '<div class="error">Errore connessione al server! Riprova...</div>'
         );
     });
 }
