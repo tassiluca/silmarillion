d16303dc9799c0f125e6e9fe7390c1d2bbd3590a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-01 23:03:44 +0100

    style sheet order to modify priority

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'bootstrap.php';
 
-    $templateParams["css"] = array("login.css", "login-forms.css");
+    $templateParams["css"] = array("login-forms.css", "login.css");
     $templateParams["js"] = array("login.js");
     $templateParams["main"] = "./templates/login-page.php";
 
