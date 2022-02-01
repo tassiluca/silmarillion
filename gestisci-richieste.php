29ad382e5d7a136d2c30bdf9c81dfef6b1bd5e8b
Matteo <matteo.violani@studio.unibo.it>
2022-02-01 12:03:51 +0100

    dispatching action request on product

--- gestisci-richieste.php
+++ gestisci-richieste.php
@@ -1,24 +1,33 @@
 <?php
     require_once 'bootstrap.php';
     session_start();  // needed for sessions.
+
+    if(isset($_SESSION['url'])){
+         $lastPage = $_SESSION['url']; // holds url for last page visited.
+    }
+    else{
+        $lastPage = "index.php"; 
+    }
+
+    //TODO: CONTROLLO DI LOGIN EFFETTUATO E STRINGA AZIONE GESTIBILE
+    //AGGIUNGERE VERIFICA CORRETTO LOGIN
+    if(isset($_GET["action"]) && isset($_GET["id"])){
+        $action = $_GET["action"];
+        $idprod = $_GET["id"];
+
+        if(!strcmp($action,'wish')){
+            //echo 'add to wishlist id'. $idprod;
+            $dbh -> addProductToWish(2,$idprod); //user and prodid
+        }
+        else if(!strcmp($action,'toCart')){
+            //echo 'add to cart id'. $idprod;
+            $dbh -> addProductToCart(2,$idprod,5); //iduser,id product, quantity
+        }
+        else if(!strcmp($action,'notify')){
+            //echo 'notify me id'. $idprod;
+            $dbh -> addAlert(2,$idprod);
+        }
+    }
     
-    if(isset($_SESSION['url'])) 
-       $lastPage = $_SESSION['url']; // holds url for last page visited.
-    else 
-       $url = "index.php"; 
-    
-    header("Location: $lastPage"); 
-//controllare se l'utente è loggato
-//Controllare azione 
-/*
-if(!isUserLoggedIn() || !isset($_GET["action"])){
-
-        //reindirizzo l'utente al login se viene passata una action non gestita
-        header("location: login.php");
-
-}*/
-
-    require 'templates/base.php';
-
-    
+    header("Location: $lastPage"); //redirect to lastpage where action was sent
 ?>
