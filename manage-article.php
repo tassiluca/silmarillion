14fa9a2b629da90813b28602f0b09c4cdbeac328
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-02 10:13:47 +0100

    added manage-article page

---
+++ manage-article.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'bootstrap.php';
+
+    $templateParams["css"] = array();
+    $templateParams["js"] = array();
+    $templateParams["main"] = "./templates/manage-article-page.php";
+
+    require 'templates/base.php';
+?>
