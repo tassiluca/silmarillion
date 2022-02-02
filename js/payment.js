7fdf2d80f5e03cc85fc832f75ad43a738d2d5323
Matteo <matteo.violani@studio.unibo.it>
2022-02-02 14:52:07 +0100

    added selection of payment method in payPage

--- js/payment.js
+++ js/payment.js
@@ -1,15 +1,16 @@
+
 function editAddress(){
     $(document).ready(function () {
-        var fields = $("main > section > div > div > article > form > ul > li:not(:first-child) > input");
-        if($("main > section > div > div > article > form > a:first-of-type").is(":visible")){
-            fields.attr('disabled', false);
-            $("main > section > div > div > article > form > a:first-of-type").hide();
-            $("main > section > div > div > article > form > a:last-of-type").show();
+        var fields = $("main > section > div > div > article > form > div:first-of-type > ul > li > input");
+        if($("main > section > div > div > article > form > div:first-of-type > a:first-of-type").is(":visible")){
+            fields.attr('readonly', false);
+            $("main > section > div > div > article > form > div:first-of-type > a:first-of-type").hide();
+            $("main > section > div > div > article > form > div:first-of-type > a:last-of-type").show();
         }
         else{
-            fields.attr('disabled', true);
-            $("main > section > div > div > article > form > a:first-of-type").show();
-            $("main > section > div > div > article > form > a:last-of-type").hide();
+            fields.attr('readonly', true);
+            $("main > section > div > div > article > form > div:first-of-type > a:first-of-type").show();
+            $("main > section > div > div > article > form > div:first-of-type > a:last-of-type").hide();
         }
     });    
 }
