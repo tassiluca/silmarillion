626450cf8161d477fed4b5c251ac83e27c4c3546
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-11 16:06:03 +0100

    Deleted css already included

--- templates/base.php
+++ templates/base.php
@@ -4,8 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="./css/style.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <?php 
+        <?php
             /**
              * Insert here all the style sheets needed by the single page.
              * To do so declare a $templateParams["css"] with an array of style sheet files name to import.
