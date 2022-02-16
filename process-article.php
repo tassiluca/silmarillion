b230b50a466b8a3f70a2e44d3e687b8f201725c7
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-16 17:56:01 +0100

    init category insert

--- process-article.php
+++ process-article.php
@@ -1,39 +1,32 @@
 <?php 
     require_once './bootstrap.php';
 
-    function validateInputs() {
-        // articleToInsert ?
-        $checkCompulsoryFields = checkInputs(array($_POST['category'], $_POST['price'], 
-            $_POST['desc'], $_FILES['coverImg']));
-        if ($_POST["articleToInsert"] == "Funko") {
-            $checkFunkoFields = checkInputs(array($_POST['funkoName']));
-            return $checkCompulsoryFields && $checkFunkoFields;
-        } else if ($_POST['articleToInsert'] == "Fumetto") {
-            $checkComicFields = checkInputs(array($_POST['title'], $_POST['author'], 
-                $_POST['language'], $_POST['publishDate'], $_POST['isbn'], $_POST['publisher']));
-            return $checkCompulsoryFields && $checkComicFields;
-        }
-    }
+    /* TODO: input validation */
 
-    if (!validateInputs()) {
-        header("location: modify-article.php?formMsg=Campi vuoti!");
-    }
-    list($result, $msg) = uploadImage(UPLOAD_DIR, $_FILES["coverImg"]);
+    $price = htmlspecialchars($_POST['price']);
+    $desc = htmlspecialchars($_POST['desc']);
+    $category = htmlspecialchars($_POST['category']);
+    $discountedPrice = htmlspecialchars($_POST['discountedPrice']);
+    list($result, $msg) = uploadImage(UPLOAD_DIR_PRODUCTS, $_FILES["coverImg"]);
     if ($result != 0) {
         $coverImg = $msg;
-        $price = htmlspecialchars($_POST['price']);
-        $desc = htmlspecialchars($_POST['desc']);
-        $category = htmlspecialchars($_POST['category']);
-        $discountedPrice = htmlspecialchars($_POST['discountedPrice']);
+
+        // TODO: to refactor...
+        /*
+        if (empty($category)) {
+            $categoryName = htmlspecialchars($_POST['categoryName']);
+            $categoryDesc = htmlspecialchars($_POST['categoryDesc']);
+            $category = $dbh->addCategory($categoryName, $categoryDesc);
+            if (!$category) {
+                $msg = "Errore inserimento categoria";
+            }
+        }
+        */
+
         if ($_POST["articleToInsert"] == "Funko") {
             $funkoName = htmlspecialchars($_POST['funkoName']);
             // insert into db
-            $res = $dbh->addFunko($funkoName, 
-                                  $price, 
-                                  $discountedPrice, 
-                                  $desc, 
-                                  $coverImg,
-                                  $category);
+            $res = $dbh->addFunko($funkoName, $price, $discountedPrice, $desc, $coverImg, $category);
             $msg = $res ? "Inserimento completato correttamente" : "Errore in inserimento";
         } else if ($_POST["articleToInsert"] == "Fumetto") {
             $title = htmlspecialchars($_POST['title']);
@@ -43,21 +36,11 @@
             $isbn = htmlspecialchars($_POST['isbn']);
             $publisher = htmlspecialchars($_POST['publisher']);
             // insert into db
-            $res = $dbh->addComic($title,
-                                  $author,
-                                  $language,
-                                  $publishDate,
-                                  $isbn,
-                                  $publisher,
-                                  $price,
-                                  $discountedPrice,
-                                  $desc,
-                                  $coverImg, 
-                                  $category);
+            $res = $dbh->addComic($title, $author, $language, $publishDate, $isbn, $publisher,
+                                  $price, $discountedPrice, $desc, $coverImg, $category);
             $msg = $res ? "Inserimento completato correttamente" : "Errore in inserimento";
         }
     }
     
     header("location: modify-article.php?formMsg=" . $msg);
-
 ?>
