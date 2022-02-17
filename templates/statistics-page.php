244b5a3145c13a1c515cd28fb28d457183d74276
Matteo <matteo.violani@studio.unibo.it>
2022-02-17 18:10:37 +0100

    html stats page structure

--- templates/statistics-page.php
+++ templates/statistics-page.php
@@ -1,9 +1,20 @@
+<div>
+    <ul>
+        <li><a href="index.php"><img src="./img/home-icon.svg" alt="Home"></a></li><li>Statistiche</li>
+    </ul>
+</div>
 <section>
     <header><h3>Statistiche</h3></header>
     <article>
         <header><H4>Incassi</H4></header>
         <p>
-            <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+            <canvas id="collChart" ></canvas>
+        </p>
+    </article>
+    <article>
+        <header><H4>Ordini</H4></header>
+        <p>
+            <canvas id="orderChart"></canvas>
         </p>
     </article>
     <footer></footer>
