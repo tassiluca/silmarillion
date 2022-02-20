455dd9fe7906a859be62dfca7469ac56e7cf138c
Matteo <matteo.violani@studio.unibo.it>
2022-02-20 16:44:40 +0100

    controll period data selection

--- utils/manage-statistics.php
+++ utils/manage-statistics.php
@@ -2,12 +2,21 @@
     require_once '../bootstrap.php';
     require_once 'functions.php';
     define("UPDATE",0);
+    define("DAY",0);
+    define("MONTH",1);
+    define("YEAR",2);
 
     if(isset($_POST) && isset($_POST['action'])/*&& isSellerLoggedIn()*/){
         if(count($_POST['action']) >= 1 && $_POST['action'][0]== UPDATE){
-            $numOrder = $dbh -> getNumOrdersPerMonth();
-            $monthIn = $dbh -> getIncashPerMonth();
-            $data = array('countOrder' => $numOrder, 'monthIn' => $monthIn);
+            if(count($_POST['period']) >= 1){
+                $numOrder = $dbh -> getNumOrdersPerPeriod($_POST['period'][0]);
+                $cashIn = $dbh -> getIncashPerPeriod($_POST['period'][0]);
+            }
+            else{
+                $numOrder = $dbh -> getNumOrdersPerPeriod();
+                $cashIn = $dbh -> getIncashPerPeriod();
+            } 
+            $data = array('countOrder' => $numOrder, 'period' => $cashIn);
             echo json_encode($data);
         }
     }
