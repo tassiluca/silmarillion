f3ae407b48b5dc26ff037cb6ea3a96a58de0b7cd
Matteo <matteo.violani@studio.unibo.it>
2022-02-24 10:43:13 +0100

    removed some comment

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
     $templateParams["sectionTitle"] = array("newArrival"=>"Nuovi Arrivi","manga" => "Manga","hero" => "Supereroi","funko" => "Funko Pop");
     $templateParams["sections"] =array_keys($templateParams["sectionTitle"]);
     /*i nomi di classi in questo array devono essere rispettati nei rispettivi templateParams per il corretto funzionamento*/
+    
     $templateParams["newArrival"] = $dbh -> getNewArrival(); //default amount of new comics is 10, specify as param desired quantity
-    $templateParams["manga"] = $dbh -> getComicsOfCategory('manga'); //ci si passa un numero in modo da poter decidere un limite di fumetti nella home,
-                                                // se non specificato allora no limite
+    $templateParams["manga"] = $dbh -> getComicsOfCategory('manga');
     $templateParams["hero"] = $dbh -> getComicsOfCategory('hero');
     $templateParams["funko"] = $dbh -> getComicsOfCategory('funko');
 
