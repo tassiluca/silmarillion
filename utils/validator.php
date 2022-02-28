99a64745879d97293c5b670a6caa1f95e162636f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-02-28 22:21:22 +0100

    messages not useful

--- utils/validator.php
+++ utils/validator.php
@@ -5,37 +5,18 @@
     
     class UserInputValidator 
     {
-
         private $rules = [];
-
-        private $messages = [];
 
         private $errors = [];
 
         public function __construct() 
         {
             $this->initRules();
-            $this->initMessages();
         }
 
         private function initRules()
         {
             $this->rules['compulsory'] = v::notEmpty()->setName('compulsory');
-        }
-
-        private function initMessages() 
-        {
-            $this->messages = [
-                'alpha'         => '{{name}} must only contain alphabetic characters.',
-                'alnum'         => '{{name}} must only contain alpha numeric characters and dashes.',
-                'numeric'       => '{{name}} must only contain numeric characters.',
-                'noWhitespace'  => '{{name}} must not contain white spaces.',
-                'length'        => '{{name}} must length between {{minValue}} and {{maxValue}}.',
-                'email'         => 'Please make sure you typed a correct email address.',
-                'creditCard'    => 'Please make sure you typed a valid card number.',
-                'date'          => 'Make sure you typed a valid date for the {{name}} ({{format}}).',
-                'password_confirmation' => 'Password confirmation doesn\'t match.'
-            ];
         }
 
         public function assert($inputs) 
