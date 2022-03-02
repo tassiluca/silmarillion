e290bc760d19f2f28b67de85fc00ec07fcde3403
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-03-02 14:47:56 +0100

    dynamic page -- insert / modify / delete

--- modify-article.php
+++ modify-article.php
@@ -7,23 +7,42 @@
     $templateParams["categories"] = $dbh->getCategories();
     $templateParams["publishers"] = $dbh->getPublishers();
 
-    // if the seller is not logged in or get request is not complete redirect to login page
-    if (!isSellerLoggedIn() || !isset($_GET['action']) || !isset($_GET['article']) ||
-        ($_GET['action'] != 'insert' && $_GET['action'] != 'modify' && $_GET['action'] != 'delete') || 
-        ($_GET['article'] != 'funko' && $_GET['article'] != 'comic') ||
-        ($_GET['action'] != 'insert' && !isset($_GET['id']))) {
+    /**
+     * [NOTES]
+     * In order to add a new product must be get-requested "action=insert" with "article=?" 
+     * where ? is 'funko' or 'comic' depending on wich type of product want to insert.
+     * In order to modify or delete a product must be get-requested "action=modify&id=?"
+     * where ? is the product's id to modify.
+     */
+
+    // if the seller is not logged in or get request is not complete as descripted above
+    // redirect the user to the login page
+    if (!isSellerLoggedIn() || !isset($_GET['action']) ||
+        ($_GET['action'] !== 'insert' && $_GET['action'] !== 'modify') ||
+        ($_GET['action'] === 'insert' && (!isset($_GET['article']) || 
+        ($_GET['article'] !== 'funko' && $_GET['article'] !== 'comic')) ||
+        ($_GET['action'] !== 'insert' && !isset($_GET['id'])))) {
             header("location: login.php");
+    } else {
+        $templateParams['article'] = $_GET['article'];
     }
 
-    if ($_GET["action"] != 'insert') {
+    if ($_GET["action"] !== 'insert') {
+        $templateParams['article'] = $dbh->isFunko($_GET['id']) ? 'funko' : 'comic';
         $productInfo = $dbh->getProduct($_GET['id']);
-        print_r($productInfo);
         if (count($productInfo) == 0) {
             $templateParams["product"] = null;
         } else {
             $templateParams["product"] = $productInfo[0];
         }
     } else {
+        $templateParams['product'] = getEmptyFormData();
+    }
+
+    function getEmptyFormData(){
+        $fields = array("Name", "Title", "Author", "Lang", "PublishDate", "ISBN", "ProductId", "PublisherId", 
+        "Price", "DiscountedPrice", "Description", "CategoryName", "PublisherName", "CoverImg");
+        return array_map(function() { return ""; }, $fields);
     }
 
     require 'templates/base.php';
--- templates/modify-article-page.php
+++ templates/modify-article-page.php
@@ -10,115 +10,121 @@
     </ul>
 </div>
 <section>
-    <header>
-        <h2>
-            <?php echo ($_GET['action'] == 'insert' ? 'Inserisci' : 'Modifica'); ?> 
-            <?php echo ($templateParams['article'] == 'funko' ? 'Funko' : 'Fumetto'); ?>
-        </h2>
-    </header>
-    <!-- Comic insertion form -->
-    <form action="process-article.php" method="POST" enctype="multipart/form-data" id="comicForm">
-        <ul>
-            <?php 
-                if ($_GET['article'] == 'comic'):
-            ?>
+    <?php if ($product == null): ?>
+        <header>
+            <h2>Articolo non trovato</h2>
+        </header>
+    <?php else: ?>
+        <header>
+            <h2>
+                <?php echo ($_GET['action'] === 'insert' ? 'Inserisci' : 'Modifica'); ?> 
+                <?php echo ($templateParams['article'] === 'funko' ? 'Funko' : 'Fumetto'); ?>
+            </h2>
+        </header>
+        <!-- Comic insertion form -->
+        <form action="process-article.php" method="POST" enctype="multipart/form-data" id="comicForm">
+            <ul>
+                <?php if ($templateParams['article'] == 'comic'): ?>
+                    <li>
+                        <input type="hidden" name="articleToInsert" value="comic" />
+                    </li>
+                    <li>
+                        <label for="title">Titolo</label>
+                        <input type="text" placeholder="es. Two Moons 1" id="title" name="title" value="<?php echo $product['Title']; ?>" required />
+                    </li>
+                    <li>
+                        <label for="author">Autore</label>
+                        <input type="text" placeholder="es. John Arcudi" id="author" name="author" value="<?php echo $product['Author']; ?>" required />
+                    </li>
+                    <li>
+                        <label for="language">Lingua</label>
+                        <input type="text" placeholder="es. Italiano" id="language" name="language" value="<?php echo $product['Lang']; ?>" required />
+                    </li>
+                    <li>
+                        <label for="publishDate">Data di pubblicazione</label>
+                        <input type="date" id="publishDate" name="publishDate" value="<?php echo $product['PublishDate']; ?>" required />
+                    </li>
+                    <li>
+                        <label for="isbn">ISBN</label>
+                        <input type="text" placeholder="es. 9781534319110" id="isbn" name="isbn" value="<?php echo $product['ISBN']; ?>" required />
+                    </li>
+                    <li>
+                        <label for="publisher">Editore</label>
+                        <input list="publishers" name="publisher" id="publisher" value="<?php echo $product['PublisherName']; ?>" />
+                        <datalist id="publishers">
+                            <?php foreach ($templateParams["publishers"] as $publisher): ?>
+                                <option value="<?php echo $publisher["PublisherId"]; ?>"><?php echo $publisher["Name"]; ?></option>
+                            <?php endforeach; ?>
+                        </datalist>
+                        <button id="addPublisherBtn" aria-label="Aggiungi Editore"></button>
+                        <ul>
+                            <li>
+                                <label for="publisherName">Nome Editore</label>
+                                <input type="text" id="publisherName" name="publisherName" placeholder="es. Panini Comics" />
+                            </li>
+                            <li>
+                                <label for="publisherLogo">Logo Editore</label>
+                                <input type="file" id="publisherLogo" name="publisherLogo" />
+                            </li>
+                        </ul>
+                    </li>
+                <?php elseif ($templateParams['article'] == 'funko') : ?>
+                    <li>
+                        <input type="hidden" name="articleToInsert" value="funko" />
+                    </li>
+                    <li>
+                        <label for="funkoName">Nome</label>
+                        <input type="text" placeholder="es. Joan Jett Pop" id="funkoName" name="funkoName" value="<?php echo $product['Name']; ?>" required />
+                    </li>
+                <?php endif; ?>
                 <li>
-                    <input type="hidden" name="articleToInsert" value="comic" />
-                </li>
-                <li>
-                    <label for="title">Titolo</label>
-                    <input type="text" placeholder="es. Two Moons 1" id="title" name="title" required />
-                </li>
-                <li>
-                    <label for="author">Autore</label>
-                    <input type="text" placeholder="es. John Arcudi" id="author" name="author" required />
-                </li>
-                <li>
-                    <label for="language">Lingua</label>
-                    <input type="text" placeholder="es. Italiano" id="language" name="language" required />
-                </li>
-                <li>
-                    <label for="publishDate">Data di pubblicazione</label>
-                    <input type="date" id="publishDate" name="publishDate" required />
-                </li>
-                <li>
-                    <label for="isbn">ISBN</label>
-                    <input type="text" placeholder="es. 9781534319110" id="isbn" name="isbn" required />
-                </li>
-                <li>
-                    <label for="publisher">Editore</label>
-                    <input list="publishers" name="publisher" id="publisher" />
-                    <datalist id="publishers">
-                        <?php foreach ($templateParams["publishers"] as $publisher): ?>
-                            <option value="<?php echo $publisher["PublisherId"]; ?>"><?php echo $publisher["Name"]; ?></option>
+                    <label for="category">Categoria</label>
+                    <input list="categories" name="category" id="category" value="<?php echo $product['CategoryName']; ?>" />
+                    <datalist id="categories">
+                        <?php foreach ($templateParams["categories"] as $category): ?>
+                            <option value="<?php echo $category['Name']; ?>"><?php echo $category['Name']; ?></option>
                         <?php endforeach; ?>
                     </datalist>
-                    <button id="addPublisherBtn" aria-label="Aggiungi Editore"></button>
+                    <button id="addCategoryBtn" aria-label="Aggiungi Categoria"></button>
                     <ul>
                         <li>
-                            <label for="publisherName">Nome Editore</label>
-                            <input type="text" id="publisherName" name="publisherName" placeholder="es. Panini Comics" />
+                            <label for="categoryName">Nome Categoria</label>
+                            <input type="text" id="categoryName" name="categoryName" placeholder="es. Manga" />
                         </li>
                         <li>
-                            <label for="publisherLogo">Logo Editore</label>
-                            <input type="file" id="publisherLogo" name="publisherLogo" />
+                            <label for="categoryDesc">Descrizione Categoria</label>
+                            <textarea id="categoryDesc" name="categoryDesc" placeholder="Descrizione della categoria: es. fumetti di piccolo formato originari del Giappone."></textarea>
                         </li>
                     </ul>
                 </li>
-            <?php
-                elseif ($_GET['article'] == 'funko') :
-            ?>
                 <li>
-                    <input type="hidden" name="articleToInsert" value="funko" />
+                    <label for="price">Prezzo</label>
+                    <input type="text" placeholder="es. 15,00" id="price" name="price" value="<?php echo $product['Price']; ?>" required />
                 </li>
                 <li>
-                    <label for="funkoName">Nome</label>
-                    <input type="text" placeholder="es. Joan Jett Pop" id="funkoName" name="funkoName" required />
+                    <label for="discountedPrice">Prezzo Scontato</label>
+                    <input type="text" placeholder="es. 14,00" id="discountedPrice" name="discountedPrice" value="<?php echo $product['DiscountedPrice']; ?>" />
                 </li>
-            <?php
-                endif;
-            ?>
-            <li>
-                <label for="category">Categoria</label>
-                <input list="categories" name="category" id="category" />
-                <datalist id="categories">
-                    <?php foreach ($templateParams["categories"] as $category): ?>
-                        <option value="<?php echo $category['Name']; ?>"><?php echo $category['Name']; ?></option>
-                    <?php endforeach; ?>
-                </datalist>
-                <button id="addCategoryBtn" aria-label="Aggiungi Categoria"></button>
-                <ul>
-                    <li>
-                        <label for="categoryName">Nome Categoria</label>
-                        <input type="text" id="categoryName" name="categoryName" placeholder="es. Manga" />
-                    </li>
-                    <li>
-                        <label for="categoryDesc">Descrizione Categoria</label>
-                        <textarea id="categoryDesc" name="categoryDesc" placeholder="Descrizione della categoria: es. fumetti di piccolo formato originari del Giappone."></textarea>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <label for="price">Prezzo</label>
-                <input type="text" placeholder="es. 15,00" id="price" name="price" required />
-            </li>
-            <li>
-                <label for="discountedPrice">Prezzo Scontato</label>
-                <input type="text" placeholder="es. 14,00" id="discountedPrice" name="discountedPrice" />
-            </li>
-            <li>
-                <label for="desc">Descrizione</label>
-                <textarea placeholder="Descrizione sintetica del prodotto" id="desc" name="desc" required ></textarea>
-            </li>
-            <li>
-                <label for="coverImg">Immagine Articolo</label>
-                <input type="file" name="coverImg" id="coverImg" required />
-                <!-- TODO <img src="" alt=""> -->
-            </li>
-            <li>
-                <!-- TODO nel caso di modifica metti modifica !-->
-                <input type="submit" value="INSERISCI" />
-            </li>
-        </ul>
-    </form>
+                <li>
+                    <label for="desc">Descrizione</label>
+                    <textarea placeholder="Descrizione sintetica del prodotto" id="desc" name="desc" required ><?php echo $product['Description']; ?>"</textarea>
+                </li>
+                <li>
+                    <?php if ($_GET['action'] == 'insert'): ?>
+                        <label for="coverImg">Immagine Articolo</label>
+                        <input type="file" name="coverImg" id="coverImg" required />
+                    <?php else: ?>
+                        <!-- TODO: alt ? -->
+                        <img src="<?php echo UPLOAD_DIR_PRODUCTS . $product['CoverImg']; ?>" alt="" />
+                    <?php endif; ?>
+                </li>
+                <li>
+                    <?php  if ($_GET['action'] !== 'insert'): ?>
+                        <input type="submit" value="Elimina" />
+                    <?php endif; ?>
+                    <input type="submit" value="<?php echo ($_GET['action'] === 'insert' ? 'Inserisci' : 'Modifica'); ?>" />
+                </li>
+            </ul>
+        </form>
+    <?php endif; ?>
 </section>
