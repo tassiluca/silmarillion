784d7d2016fa7cda78c99ffc252dbaf3024a2b29
Matteo <matteo.violani@studio.unibo.it>
2022-03-02 17:37:06 +0100

    get correct nu  copies available prod

--- article.php
+++ article.php
@@ -7,10 +7,12 @@
     $templateParams["main"] = "./templates/single-article.php";
 
     $idprodotto = -1;
-    if(isset($_GET["comicId"])){
+    if(isset($_GET["prodId"])){
+        $idprodotto = $_GET["prodId"];
         $templateParams["product"] = $dbh -> getComicById($_GET["comicId"])[0];
     }
     else if(isset($_GET["funkoId"])){
+        $idprodotto = $_GET["funkoId"];
         $templateParams["product"] = $dbh -> getFunkoById($_GET["funkoId"])[0];
     }
     
