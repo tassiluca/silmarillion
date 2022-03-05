1cfa380213509648ccaded908502f6d49eadb6e2
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-03-05 13:19:02 +0100

    reporting all errors

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,10 @@
 <?php
+
+    /* Reporting all errors */
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     session_start();
 
     define("UPLOAD_DIR_NEWS", "./upload/news/");
