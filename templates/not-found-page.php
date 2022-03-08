a9539edebb0f6745e87d8633af0d1548875a9ba9
Matteo <matteo.violani@studio.unibo.it>
2022-03-08 22:29:35 +0100

    added somethings erro page

--- templates/not-found-page.php
+++ templates/not-found-page.php
@@ -2,13 +2,19 @@
  <div>
     <ul>
         <li><a href="index.php"><img src="./img/home-icon.svg" alt="Home"></a></li>
+        <li>Pagina errore</li>
     </ul>
 </div>
 <section>
     <header>
-        <h2>Sei c091i0n3 hai fatto un marone !!</h2>
+        <h3>Sei c091i0n3 hai fatto un marone !!</h3>
     </header>
     <div>
-        <img src="" alt="">
+        <?php
+            $images = glob("./img/not-found/*");
+            $imgAmount = count($images)-1 < 0 ? 0 : count($images)-1;
+        ?>
+        <div><p>Wolf Wolf !!</p></div>
+        <img src="<?php echo $images[random_int(0,$imgAmount)]?>" alt="">
     </div>
 </section>
