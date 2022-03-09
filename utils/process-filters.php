68cd437eaaaf817a4a427032dc452ec27fb3dcfa
Matteo <matteo.violani@studio.unibo.it>
2022-03-09 11:36:30 +0100

    init method to add 'is favourite' info to prods

--- utils/process-filters.php
+++ utils/process-filters.php
@@ -184,6 +184,20 @@ filters = lang, author, price, availability, publisher,category*/
         return $allProd;
     }
 
+    function addIsFavouriteInfo($prods,$dbh){
+        $allProd = $prods;
+        for($i=0; $i < count($prods);$i++){
+            if(isCustomerLoggedIn()){
+                print_r($_SESSION['userId']);
+                $allProd[$i]["isFavourite"] = $dbh -> isFavourite($_SESSION['userId'],$allProd[$i]["ProductId"]);
+            }
+            else if(true){
+
+            }
+        }
+        return $allProd;
+    }
+
     function applyFilterAvailable($prods,$selFilter){
         $allProd = array();
         for($i=0; $i < count($prods);$i++){
@@ -212,6 +226,7 @@ filters = lang, author, price, availability, publisher,category*/
             $comics = $dbh -> getAllComicsMatch($varTypes,$varArray,$query);
             $prods = array_merge($funkos,$comics);
         }
+        $prods = addIsFavouriteInfo($prods,$dbh);
         $prods = addAvaiableCopiesInfo($prods,$dbh);
         $prods = applyFilterAvailable($prods,$avail);
         echo json_encode($prods); //before send json add numCopies info foreach products
