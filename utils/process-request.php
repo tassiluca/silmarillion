2e0136adfdcc8499f1aa1242f0bc41185b39e277
Matteo <matteo.violani@studio.unibo.it>
2022-03-09 16:59:46 +0100

    new methiod to handle request from logged customer

--- utils/process-request.php
+++ utils/process-request.php
@@ -9,38 +9,39 @@
     }
 
     if(isset($_GET["action"]) && isset($_GET["id"])){
+        $action = $_GET["action"];
+        $idprod = $_GET["id"];
         if(isCustomerLoggedIn()){
-            $action = $_GET["action"];
-            $idprod = $_GET["id"];
-            $idCustomer = $_SESSION['userId'];
-            
-            if(!strcmp($action,'wish')){
-                $dbh -> addProductToWish($idCustomer,$idprod);
-            }
-            else if(!strcmp($action,'addtoCart')){
-                $dbh -> addProductToCart($idCustomer,$idprod,1);
-            }
-            else if(!strcmp($action,'decToCart')){
-                //decrement quantity of product in cart
-                $dbh -> decrementProductToCart($idCustomer,$idprod,1);
-            }
-            else if(!strcmp($action,'delFromCart')){
-                //completely remove product from cart in db
-                $dbh -> deleteProductFromCart($idCustomer,$idprod,1);
-            }
-            else if(!strcmp($action,'notify')){
-                //echo 'notify me id'. $idprod;
-                $dbh -> addProductAlert($idCustomer,$idprod);
-            }
+            handleLoggedCustomerRequest($action,$_SESSION['userId'],$idprod);
         }
         else if(isset($_COOKIE['userData'])){ //case where cookie ha been already setted--> append new infos
 
         }
-        else if(!isset($_COOKIE['userData'])){ // first time we save cart and favourite costumer data in cookie
-            
+        else if(!isset($_COOKIE['userData'])){ //first time we save cart and favourite costumer data in cookie
+
         }   
     }
 
+    function handleLoggedCustomerRequest($action,$idCustomer,$idprod){
+        if(!strcmp($action,'wish')){
+            $dbh -> addProductToWish($idCustomer,$idprod);
+        }
+        else if(!strcmp($action,'addtoCart')){
+            $dbh -> addProductToCart($idCustomer,$idprod,1);
+        }
+        else if(!strcmp($action,'decToCart')){
+            //decrement quantity of product in cart
+            $dbh -> decrementProductToCart($idCustomer,$idprod,1);
+        }
+        else if(!strcmp($action,'delFromCart')){
+            //completely remove product from cart in db
+            $dbh -> deleteProductFromCart($idCustomer,$idprod,1);
+        }
+        else if(!strcmp($action,'notify')){
+            //echo 'notify me id'. $idprod;
+            $dbh -> addProductAlert($idCustomer,$idprod);
+        }
+    }
     
     
    header("Location: $lastPage"); //redirect to lastpage where action was sent
