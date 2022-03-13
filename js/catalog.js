bc2bd00d18d08152ec9bde5eefd73e3c15a7d096
Matteo <matteo.violani@studio.unibo.it>
2022-03-13 19:09:02 +0100

    moved utils funciton to file utils.js

--- js/catalog.js
+++ js/catalog.js
@@ -14,22 +14,6 @@ function hideElement(element) {
         .next().slideUp();
 }
 
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-    return false;
-};
-
 $(document).ready(function(){
     firstLoadProds();
 
