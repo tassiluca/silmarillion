4f0f646425e9969f1e989c229a8b8bc292acac6a
Matteo <matteo.violani@studio.unibo.it>
2022-03-13 19:43:30 +0100

    specific js to handle button click  of prod

---
+++ js/product-actions.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    if(getCookie("favs") == ""){
+        empty = [];
+        setCookie("favs", JSON.stringify(empty), 30);
+    }
+    //Favourite button
+    $('main > section > div > article > footer > div:first-child > a').click(function (e) { 
+        e.preventDefault();
+        urlRequest = $(this).attr("href");
+        var prodId = parseInt(getUrlParameter("id",urlRequest));
+        updateWishlist(prodId);
+        //TODO: check if logged, if not use cookies
+        //if customer is logged --> ajax request --> server apply to db --> return status of operation to client javascript
+    });
+
+    //add to cart button
+    $('main > section > div > article > footer > div:nth-child(2) > a').click(function (e) { 
+        e.preventDefault();
+        console.log('cart');
+    });
+});
