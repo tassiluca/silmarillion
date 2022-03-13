c1d537b246d118262c1bb4cd64b8c75ea87b337f
Matteo <matteo.violani@studio.unibo.it>
2022-03-13 18:55:25 +0100

    set and get cookie cuntion utility

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,10 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
+} 
+
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -13,3 +20,21 @@ function getCookie(cname) {
     }
     return "";
   }
+
+function updateWishlist(idProd){
+    strCookie = getCookie('favs'); //prendo cookie che mi interessa
+    curWishlist =JSON.parse(strCookie); //lo converto in oggetto javascript
+
+    if(curWishlist.includes(idProd)){
+    curWishlist.splice(curWishlist.indexOf(idProd));
+    }
+    else{
+    curWishlist.push(idProd);
+    }
+
+    var json_str = JSON.stringify(curWishlist);
+    setCookie('favs', json_str,30);
+
+    console.log(getCookie('favs'));
+}
+  
