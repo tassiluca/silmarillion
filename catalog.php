30c295674d0253ca47fc5f6be8097088b12fbcd6
Matteo <matteo.violani@studio.unibo.it>
2022-03-16 15:29:15 +0100

    global declaration of dbh

--- catalog.php
+++ catalog.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'bootstrap.php';
     $_SESSION['url'] = $_SERVER['REQUEST_URI'];
+    global $dbh;
     const CATALOG_PROD_QUANTITY = 25;
     
     $templateParams["css"] = array("./css/catalog.css","./css/products.css");
