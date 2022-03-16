41ba5b1af5b4955df831727c02363b1cdc4af17d
Matteo <matteo.violani@studio.unibo.it>
2022-03-16 09:25:12 +0100

    define to const catalog

--- catalog.php
+++ catalog.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'bootstrap.php';
-    $_SESSION['url'] = $_SERVER['REQUEST_URI']; 
-    define("CATALOG_PROD_QUANTITY",25);
+    $_SESSION['url'] = $_SERVER['REQUEST_URI'];
+    const CATALOG_PROD_QUANTITY = 25;
     
     $templateParams["css"] = array("./css/catalog.css","./css/products.css");
     $templateParams["js"] = array("./js/catalog.js","./js/utils.js","./js/product-actions.js");
