50491377b200a494bd6a344530fe770be70251a9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-03-17 22:58:08 +0100

    aligned bootstrap. Global UPLOAD dir

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-
     /* Reporting all errors */
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
@@ -7,11 +6,15 @@
 
     session_start();
 
-    /**
-     * The directory in which store the products images.
-     */
-    define("UPLOAD_DIR_PRODUCTS", "./upload/products/");
-    define("UPLOAD_DIR_PUBLISHERS", "./upload/publishers/");    
+    define("UPLOAD_DIR", "./upload");
+    /** The directory where store the products images. */
+    define("UPLOAD_DIR_PRODUCTS", UPLOAD_DIR . "/products/");
+    /** The directory where store the publishers logos. */
+    define("UPLOAD_DIR_PUBLISHERS", UPLOAD_DIR . "/publishers/");
+    /** The directory where store the banners. */
+    define("UPLOAD_DIR_BANNER", UPLOAD_DIR."/banner/");
+    /** The directory where store the news. */
+    define("UPLOAD_DIR_NEWS", UPLOAD_DIR."/news/");
 
     require_once __DIR__ . '/vendor/autoload.php';
     require_once 'db/database.php';
@@ -19,17 +22,12 @@
 
     // secureSessionStart();    // **TODO**
 
-    define("HOST", "localhost");
-    define("USER", "root");
-    define("PASSWORD", "root");
-    define("DB_NAME", "silmarillion");
-    define("DB_PORT", 3306);
+    const HOST = "localhost";
+    const USER = "root";
+    const PASSWORD = "root";
+    const DB_NAME = "silmarillion";
+    const DB_PORT = 3306;
 
     $dbh = new DatabaseHelper(HOST, USER, PASSWORD, DB_NAME, DB_PORT);
 
-    define("UPLOAD_DIR", "./upload");
-    define("PRODUCTS_DIR", UPLOAD_DIR."/products/");
-    define("PUBLISHER_DIR", UPLOAD_DIR."/publishers/");
-    define("BANNER_DIR", UPLOAD_DIR."/banner/");
-    define("UPLOAD_DIR_NEWS", UPLOAD_DIR."/news/");
 ?>
