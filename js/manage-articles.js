e1fcb98e642dbbdfde4edf0b626e2bf0a5d19c39
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-03-17 12:07:25 +0100

    init filtering

--- js/manage-articles.js
+++ js/manage-articles.js
@@ -8,5 +8,11 @@ $(document).ready(function(){
             $("ul#products").html(data);
         });
     });
+
+    $("main > section > header > input").keyup(function(){
+        $.get("test.php", {pattern : $(this).val()}, function(data){
+            $("ul#products").html(data);
+        });
+    });
     
 });
