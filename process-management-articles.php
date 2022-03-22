25d8ba8b9e54a513fa45328f6fc1dd46d4f9cc0e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-03-22 11:29:47 +0100

    refactore common code

--- process-management-articles.php
+++ process-management-articles.php
@@ -4,29 +4,22 @@
 
     const PRODUCTS_PER_PAGE = 10;
 
-    if (isset($_GET['pattern'], $_GET['page'])):
+    if (isset($_GET['pattern']) || isset($_GET['page'])) {
         $offset = ($_GET['page'] - 1) * PRODUCTS_PER_PAGE;
-        $products = $dbh->getProducts($offset, PRODUCTS_PER_PAGE, $_GET['pattern']);
+        $products = (isset($_GET['pattern']) ?
+            $dbh->getProducts($offset, PRODUCTS_PER_PAGE, $_GET['pattern']) :
+            $dbh->getProducts($offset, PRODUCTS_PER_PAGE));
         for ($i = 0; $i < count($products); $i++) {
             $products[$i]['CoverImg'] = UPLOAD_DIR_PRODUCTS . $products[$i]['CoverImg'];
         }
-        $totalAmountOfProducts = count($dbh->getProducts(expression: $_GET['pattern']));
+        $totalAmountOfProducts = count(isset($_GET['pattern']) ?
+            $dbh->getProducts(expression: $_GET['pattern']) :
+            $dbh->getProducts());
         $totalPages = ceil($totalAmountOfProducts / PRODUCTS_PER_PAGE);
         echo json_encode(array(
             'products' => $products,
             'pages' => $totalPages
         ));
-    elseif (isset($_GET['page'])) :
-        $offset = ($_GET['page'] - 1) * PRODUCTS_PER_PAGE;
-        $products = $dbh->getProducts($offset, PRODUCTS_PER_PAGE);
-        for ($i = 0; $i < count($products); $i++) {
-            $products[$i]['CoverImg'] = UPLOAD_DIR_PRODUCTS . $products[$i]['CoverImg'];
-        }
-        $totalAmountOfProducts = count($dbh->getProducts());
-        $totalPages = ceil($totalAmountOfProducts / PRODUCTS_PER_PAGE);
-        echo json_encode(array(
-            'products' => $products,
-            'pages' => $totalPages
-        ));
-    endif;
+    }
+
 ?>
