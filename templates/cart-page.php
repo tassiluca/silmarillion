83b4ce8446f505861ce6cfaf322cc6d82c004df1
Matteo <matteo.violani@studio.unibo.it>
2022-04-08 14:32:04 +0200

    new cart php page

---
+++ templates/cart-page.php
@@ -0,0 +1,40 @@
+
+<!-- Breadcrumb pagination -->
+<div>
+    <ul>
+        <li><a href="index.html"><img src="./img/home-icon.svg" alt="Home"></a></li><li>Carrello</li>
+    </ul>
+</div>
+<section>
+    <header><h2>Carrello</h2></header>
+    <div>
+        <div>
+            <!-- insert foreach product in cart an article-->
+            <article>
+                <div><img src="img/fumetto.svg" alt="copertina fumetto"></div>
+                <div>
+                    <header><h3>Titolo Fumetto</h3></header>
+                    <p>30,00 €</p>
+                </div>
+                <div>
+                    <div>
+                        <!--here we call php page to add or edit quantity of an article sending a query to specify which product and the action to do-->
+                        <div><a href="#"><img src="img/cart/subtract.svg" alt="riduci quantità"></a></div>
+                        <div><p>2</p></div>
+                        <div><a href="#"><img src="img/cart/plus_math.svg" alt="aumenta quantità"></a></div>
+                    </div>
+                    <div><a href="">Rimuovi</a></div>
+                </div>
+            </article>
+            <!-- end of article list-->
+        </div>
+        <div>
+            <p>Totale:</p><p>94,89€</p>
+        </div>
+        <div><a href="payment.html">PROCEDI ALL'ORDINE</a></div>
+    </div>
+</section>
+
+<!--
+
+-->
