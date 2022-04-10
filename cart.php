c876547d545dd6154d517355c85623d95e64766a
Matteo <matteo.violani@studio.unibo.it>
2022-04-10 18:57:38 +0200

    loading cart prods if logged

--- cart.php
+++ cart.php
@@ -1,12 +1,55 @@
 <?php
     require_once __DIR__ . '/bootstrap.php';
+    require_once 'utils/functions.php';
     global $dbh;
+    const cookie = 0;
+    const db_customer = 1;
+
     $_SESSION['url'] = $_SERVER['REQUEST_URI']; 
     
-    $templateParams["css"] = array("./css/cart.css");
+    $templateParams["css"] = array("./css/cart.css","./css/cart-payment.css");
     $templateParams["main"] = "./templates/cart-page.php";
     $templateParams["js"] = array("./js/utils.js","./js/product-actions.js");
 
+    $prodsInCart = array();
+
+    if(isCustomerLoggedIn()){
+        $prodsInCart = getInfoProdsInCart($dbh->getUserCart($_SESSION['userId']),db_customer);
+    }
+    else if(isset($_COOKIE['cart'])){
+        $prodsInCart = getInfoProdsInCart(json_decode($_COOKIE['cart']),cookie);
+    }
+
+    $templateParams["cart"] = $prodsInCart;
     
-    
+    function getInfoProdsInCart($prodsIdCart,$dataSource){
+        global $dbh;
+        $prods = array();
+        foreach($prodsIdCart as $prod){
+            if($dataSource == cookie){
+                $elem = $dbh -> getProduct($prod[0]);
+                $elem["Quantity"] = $prod[1];
+                array_push($prods,$elem);
+            }
+            else if($dataSource == db_customer){
+                $elem = $dbh -> getProduct($prod['ProductId']);
+                $elem['Quantity'] = $prod['Quantity'];
+                array_push($prods,$elem);
+            }
+        }
+        return $prods;
+    }
+
     require 'templates/base.php';
+
+/* COOKIE
+    array(2) { 
+        [0]=> array(2) { [0]=> int(18) [1]=> int(2) }
+        [1]=> array(2) { [0]=> int(12) [1]=> int(1) } 
+    } 
+
+    array(3) { [0]=> array(3) { ["ProductId"]=> int(2) ["UserId"]=> int(7) ["Quantity"]=> int(1) }
+               [1]=> array(3) { ["ProductId"]=> int(12) ["UserId"]=> int(7) ["Quantity"]=> int(1) }
+               [2]=> array(3) { ["ProductId"]=> int(18) ["UserId"]=> int(7) ["Quantity"]=> int(1) } } 
+*/
+ 
--- templates/cart-page.php
+++ templates/cart-page.php
@@ -2,7 +2,7 @@
 <!-- Breadcrumb pagination -->
 <div>
     <ul>
-        <li><a href="index.html"><img src="./img/home-icon.svg" alt="Home"></a></li><li>Carrello</li>
+        <li><a href="index.php"><img src="./img/commons/home-icon.svg" alt="Home"></a></li><li>Carrello</li>
     </ul>
 </div>
 <section>
@@ -10,28 +10,50 @@
     <div>
         <div>
             <!-- insert foreach product in cart an article-->
+            <?php 
+                $totalOrderPrice = 0.0;
+                if(isset($templateParams["cart"]) && count($templateParams["cart"]) > 0){
+                    foreach($templateParams["cart"] as $prod):
+            ?>
             <article>
-                <div><img src="img/fumetto.svg" alt="copertina fumetto"></div>
+                <div><img src="<?php echo UPLOAD_DIR_PRODUCTS.$prod['CoverImg']?>" alt="copertina fumetto"></div>
                 <div>
-                    <header><h3>Titolo Fumetto</h3></header>
-                    <p>30,00 €</p>
+                    <header><h3><?php echo $prod['Title']?></h3></header>
+                    <p><?php 
+                            if(isset($prod['DiscountedPrice'])){
+                                echo $prod['DiscountedPrice'];
+                                $totalOrderPrice += $prod['DiscountedPrice'];
+                            }
+                            else{
+                                echo $prod['Price'];
+                                $totalOrderPrice += $prod['Price'];
+                            }?> €</p>
                 </div>
                 <div>
                     <div>
                         <!--here we call php page to add or edit quantity of an article sending a query to specify which product and the action to do-->
                         <div><a href="#"><img src="img/cart/subtract.svg" alt="riduci quantità"></a></div>
-                        <div><p>2</p></div>
+                        <div><p><?php echo $prod['Quantity']?></p></div>
                         <div><a href="#"><img src="img/cart/plus_math.svg" alt="aumenta quantità"></a></div>
                     </div>
-                    <div><a href="">Rimuovi</a></div>
+                    <div><a href="./engines/process-request.php?action=delFromCart&id=<?php echo $prod['ProductId']?>">Rimuovi</a></div>
                 </div>
             </article>
+            <?php 
+                    endforeach;
+                }
+                else{
+                    echo "<p>Carrello vuoto</p>";
+                }    
+            ?>
             <!-- end of article list-->
         </div>
         <div>
-            <p>Totale:</p><p>94,89€</p>
+            <p>Totale:</p><p><?php echo $totalOrderPrice?> €</p>
         </div>
-        <div><a href="payment.html">PROCEDI ALL'ORDINE</a></div>
+        <div><a <?php 
+                    if(isset($templateParams["cart"]) && count($templateParams["cart"]) <= 0){echo "class=disabled";}
+                ?> href="payment.html">PROCEDI ALL'ORDINE</a></div>
     </div>
 </section>
 
