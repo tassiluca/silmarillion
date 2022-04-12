7be2023c00ad019360f9344bc75b121674ae68f9
Matteo <matteo.violani@studio.unibo.it>
2022-04-12 19:50:28 +0200

    refresh total price js cart

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,27 @@ $(document).ready(function () {
     refreshCart();
 });
 
+function refreshTotalPrice(){
+    $("#totalPrice").text('');
+    $.post("./engines/process-cart.php?request=getCart", function (data) {
+        let cart = JSON.parse(data);
+        let totalPrice = 0;
+        for(let i=0 ;i< cart.length;i++){
+            let prod = cart[i];
+            price = prod['DiscountedPrice'] !==null ? prod['DiscountedPrice'] : prod['Price'];
+            totalPrice += (price*prod['Quantity']);
+        }
+        $("#totalPrice").text(totalPrice+" €");
+    });
+}
+
+function checkIfEmptyRefreshCart(actualcartCount){
+    if(actualcartCount <= 0){
+        $("section > div > div:first-child()").append("<p>Carrello vuoto</p>");
+    }
+}
+
+
 function refreshCart(){
     let htmlNavBarCart = "";
 
@@ -13,7 +34,6 @@ function refreshCart(){
         cart = JSON.parse(data);
 
         if(cart.length > 0){
-//
             for(let i=0 ;i< cart.length;i++){
                 let prod = cart[i];
                 price = prod['DiscountedPrice'] !==null ? prod['DiscountedPrice'] : prod['Price'];
@@ -42,37 +62,8 @@ function refreshCart(){
         }
         
         $("section > div > div:first-child()").append(htmlNavBarCart);
+        addEventListenerButton('.removeCart',handleRemoveCartAction);//remove prod from cart
+        addEventListenerButton('.cartButtonDec',handleCartAction);//decrement quantity prod cart
+        addEventListenerButton('.cartButton',handleCartAction);//increment quantity prod cart
     });
 }
-
-/*
-
-<?php 
--                $totalOrderPrice = 0.0;
--                if(isset($templateParams["cart"]) && count($templateParams["cart"]) > 0){
--                    foreach($templateParams["cart"] as $prod):
--            ?>
--            <article id="<?php echo $prod['ProductId']?>">
--                <div><img src="<?php echo UPLOAD_DIR_PRODUCTS.$prod['CoverImg']?>" alt="copertina fumetto"></div>
--                <div>
--                    <header><h3><?php echo $prod['Title']?></h3></header>
--                    <p> €</p>
--                </div>
--                <div>
--                    <div>
--                        <!--here we call php page to add or edit quantity of an article sending a query to specify which product and the action to do-->
--                        <div><a class="cartButtonDec" href="./engines/process-request.php?action=decToCart&id=<?php echo $prod['ProductId']?>"><img src="img/cart/subtract.svg" alt="riduci quantità"></a></div>
--                        <div><p><?php echo $prod['Quantity']?></p></div>
--                        <div><a class="cartButton" href="./engines/process-request.php?action=addtoCart&id=<?php echo $prod['ProductId']?>"><img src="img/cart/plus_math.svg" alt="aumenta quantità"></a></div>
--                    </div>
--                    <div><a class="removeCart" href="./engines/process-request.php?action=delFromCart&id=<?php echo $prod['ProductId']?>">Rimuovi</a></div>
--                </div>
--            </article>
--            <?php 
--                    endforeach;
--                }
--                else{
--                    echo "<p>Carrello vuoto</p>";
--                }    
--            ?>
-*/
