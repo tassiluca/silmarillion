29bc77e41a5422d5d46f6a02f2e56dfa68a2d148
Matteo <matteo.violani@studio.unibo.it>
2022-04-12 15:51:00 +0200

    showing last two products in cart on navbar cart

--- js/navbar.js
+++ js/navbar.js
@@ -65,26 +65,36 @@ function toggleNavbar(btnPressed, elementToSlide){
 
 function refreshCartNavbar(){
     let htmlNavBarCart = "";
+    let amountProds = 2;
+    $("#navCart > ul > p").remove();
     $("#navCart > ul > li").remove();
 
     $.post("./engines/process-cart.php?request=navBar", function (data) {
-        console.log(data);
+        //console.log(data);
         cart = JSON.parse(data);
-        
-        cart.forEach(prod => {
-            price = prod['DiscountedPrice'] !=null ? prod['DiscountedPrice'] : prod['Price'];
-            htmlNavBarCart += `
-                <li>
-                    <img src="`+UPLOAD_DIR_PRODUCTS+prod['CoverImg']+`" alt="">
-                    <div>
-                        <p>`+prod['Title']+`</p>
+
+        if(cart.length > 0){
+            for(let i = cart.length-1; i >= 0 && i > cart.length-amountProds-1;i--){
+                let prod = cart[i];
+                console.log(prod);
+                price = prod['DiscountedPrice'] !==null ? prod['DiscountedPrice'] : prod['Price'];
+                htmlNavBarCart += `
+                    <li>
+                        <img src="`+UPLOAD_DIR_PRODUCTS+prod['CoverImg']+`" alt="">
                         <div>
-                            <p>`+price+` €</p>
-                            <p>x`+prod["Quantity"]+`</p>
+                            <p>`+prod['Title']+`</p>
+                            <div>
+                                <p>`+price+` €</p>
+                                <p>x`+prod["Quantity"]+`</p>
+                            </div>
                         </div>
-                    </div>
-                </li>`;
-        });
+                    </li>`;
+            }
+        }
+        else{
+            htmlNavBarCart += "<p>Carrello vuoto</p>";
+        }
+        
         $("#navCart > ul").append(htmlNavBarCart);
     });
 
