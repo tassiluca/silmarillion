1eef483aa4d4b7dfe66d0022f2b77bb48a2a0011
Matteo <matteo.violani@studio.unibo.it>
2022-04-13 15:54:00 +0200

    fix dim partners

--- js/home.js
+++ js/home.js
@@ -40,7 +40,7 @@ function updateBanner(n) {
 }
 
 function updatePartner(n) {
-  showDivs(slideIndex += n,"main > section:last-child > div > div > a",checkScreenSize(150,0.01));
+  showDivs(slideIndex += n,"main > section:last-child > div > div > a",checkScreenSize(200,0.01));
 }
 
 function updateReview(n) {
