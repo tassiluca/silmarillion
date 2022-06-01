d2846a2c1da36591607a9aa72b93c313c00bb631
Luana <luigina.mennuti2@studio.unibo.it>
2022-06-01 17:11:41 +0200

    deve funzionare l'aggiunta al carrello della wishlist

--- templates/user-wishlist-template.php
+++ templates/user-wishlist-template.php
@@ -20,23 +20,18 @@
             <th></th>
         </tr>
 
-        <!-- ciclo for che itera tutti gli elementi nella wishlist dal db
-                nel templ param
-
-
-        -->
         <?php
         if (!empty($templateParams)) {
-            foreach($templateParams["fav-elem"] as $prod):?>
-
+            foreach($templateParams["fav-elem"] as $prod):
+                ?>
 
             <tr>
                 <td><img src="../img/user-page/Delete.svg"  alt="" class="delete"></td>
                 <td><img src="<?php echo UPLOAD_DIR_PRODUCTS.$prod['CoverImg'] ?>" alt="" /></td>
-                <th><?php echo $prod['Description'] ?></th>
-                <td><?php echo $prod['Price'] ?> <p><?php echo $prod['DiscountedPrice'] ?> </p></td>
+                <th><?php echo $prod['Name'] ?></th>
+                <td><?php echo $prod['Price'] ?><p><?php echo $prod['DiscountedPrice'] ?> </p></td>
                 <td>In Stock<p class="miniText">Quantità: <?php echo $prod['copies'] ?> pz</p></td>
-                <td><button class="whiteBtn" type="button">Aggiungi al carrello</button></td>
+                <td><button class="whiteBtn" type="button"><a href="../user-wishlist.php?action=addtoCart&id=<?php echo $prod['ProductId']?>">Aggiungi al carrello</a></button></td>
             </tr>
 
             <?php endforeach;
--- user-wishlist.php
+++ user-wishlist.php
@@ -9,5 +9,16 @@ $templateParams["main"] = "./templates/user-wishlist-template.php";
 $templateParams["fav-elem"] = getFavourites($_SESSION["userId"], $dbh);
 
 
+if(isset($_GET)) {
+
+// print_r($_GET);
+    if($_GET['action'] === 'addtoCart' && isset($_GET['id']) ){
+        $dbh->insertProdCart($_SESSION['userId'], $_GET['id'], 1);
+    }
+
+}
+
+
+
 require 'templates/base.php';
 ?>
